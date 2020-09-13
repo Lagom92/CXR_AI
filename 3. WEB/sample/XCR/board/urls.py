c26ba2c8e0c2b2c492cce8a403f8c85b4f121f60
Lagom92 <jyyt0147@gmail.com>
2020-09-13 20:39:17 +0900

    Edit

--- 3. WEB/sample/XCR/board/urls.py
+++ 3. WEB/sample/XCR/board/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.test, name='test'),
     path('result/', views.result, name='result'),
-    path('detect/', views.detect, name='detect'),
-    
+    path('detect/', views.detect, name='detect'), 
 ]
