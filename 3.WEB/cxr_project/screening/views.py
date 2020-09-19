2a45b2b4ccc9ab17216a6ab0dcf92166a7209a53
Lagom92 <jyyt0147@gmail.com>
2020-09-19 14:41:07 +0900

    Complete search 선별 진료소 기능

--- 3.WEB/cxr_project/screening/views.py
+++ 3.WEB/cxr_project/screening/views.py
@@ -6,18 +6,27 @@ from .models import Center
 def list(request):
     centers = Center.objects.all()
 
+    q = request.GET.get('q', '')
+    if q: 
+        centers = centers.filter(address__icontains=q)
+
     # Pagination
-    paginator = Paginator(centers, 60) 
-    page = request.GET.get('page')
-    try:
-        screenings = paginator.page(page)
-    except PageNotAnInteger:
-        screenings = paginator.page(1)
-    except EmptyPage:
-        screenings = paginator.page(paginator.num_pages)
-    
-    context = {
-        'screenings': screenings
-    }
+    if len(centers) <= 60:
+        context = {
+            'screenings': centers
+        }
+    else:
+        paginator = Paginator(centers, 60) 
+        page = request.GET.get('page')
+        try:
+            screenings = paginator.page(page)
+        except PageNotAnInteger:
+            screenings = paginator.page(1)
+        except EmptyPage:
+            screenings = paginator.page(paginator.num_pages)
+        
+        context = {
+            'screenings': screenings
+        }
 
     return render(request, 'list.html', context)
