14f034cc598c090ef7512f8644197a8e502bebf7
joonbeom choi <cjb>
2020-09-20 16:26:55 +0900

    loginout

--- 3.WEB/cxr_project/accounts/views.py
+++ 3.WEB/cxr_project/accounts/views.py
@@ -1,3 +1,88 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
+ERROR_MSG = {
+    'ID_EXIST': '이미 사용 중인 아이디 입니다.',
+    'ID_NOT_EXIST': '존재하지 않는 아이디 입니다',
+    'ID_PW_MISSING': '아이디와 비밀번호를 다시 확인해주세요.',
+    'PW_CHECK': '비밀번호가 일치하지 않습니다',
+}
+
+def signup(request):
+
+    context = {
+        'error': {
+            'state': False,
+            'msg': ''
+        },
+    }
+    
+    if request.method == 'POST':
+
+        user_id = request.POST['user_id']
+        user_pw = request.POST['user_pw']
+        user_pw_check = request.POST['user_pw_check']
+
+        user = User.objects.filter(username=user_id)
+
+        if (user_id and user_pw):
+            if (user_pw == user_pw_check):
+
+                user = User.objects.create_user(
+                    username=user_id,
+                    password=user_pw
+                )
+
+                auth.login(request, user)
+
+                return redirect('main')
+            else:
+                context['error']['state'] = True
+                context['error']['msg'] = ERROR_MSG['PW_CHECK']
+        else:
+            context['error']['state'] = True
+            context['error']['msg'] = ERROR_MSG['ID_EXIST']
+    else:
+        context['error']['state'] = True
+        context['error']['msg'] = ERROR_MSG['ID_PW_MISSING']
+
+    return render(request, 'signup.html', context)
+    
+def login(request):
+    context = {
+        'error': {
+            'state': False,
+            'msg': ''
+        },
+    }
+    if request.method == 'POST':
+        user_id = request.POST['user_id']
+        user_pw = request.POST['user_pw']
+
+        user = User.objects.filter(username=user_id)
+        if (user_id and user_pw):
+            if len(user) != 0:
+
+                user = auth.authenticate(
+                    username=user_id,
+                    password=user_pw
+                )
+                if user != None:
+                    auth.login(request, user)
+                    return redirect('main')
+                else:
+                    context['error']['state'] = True
+                    context['error']['msg'] = ERROR_MSG['PW_CHECK']
+            else:
+                context['error']['state'] = True
+                context['error']['msg'] = ERROR_MSG['ID_NOT_EXIST']
+        else:
+            context['error']['state'] = True
+            context['error']['msg'] = ERROR_MSG['ID_PW_MISSING']
+    return render(request, 'login.html', context)
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+    return redirect('main')
