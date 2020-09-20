ce60c8fb0007568efa24b5c5d8dee0441f32b894
whiteBerryJ <wjdwlsrbs77@naver.com>
2020-09-20 21:30:13 +0900

    loader

--- 3.WEB/sample/XCR/board/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Xray(models.Model):
-    title = models.CharField(max_length=100)
-    photo = models.ImageField(blank=True, null=True, upload_to="img/%Y%m%d")
-    
-    def __str__(self):
-        return self.title
