40234811e54e9f4ef6129668e66ccfec0f1979d4
Lagom92 <jyyt0147@gmail.com>
2020-09-20 21:44:43 +0900

    Create history and detail

--- 3.WEB/cxr_project/boards/urls.py
+++ 3.WEB/cxr_project/boards/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('test/', views.test, name='test'),
     path('result/', views.result, name='result'),
     path('model/', views.model, name='model'),
+    path('history/<int:user_id>/', views.history, name='history'),
+    path('history/<int:user_id>/deatil/<int:id>/', views.detail, name='detail'),
 ]
--- 3.WEB/cxr_project/boards/views.py
+++ 3.WEB/cxr_project/boards/views.py
@@ -1,10 +1,12 @@
 from django.http import request
 from django.shortcuts import render, redirect
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.contrib.auth.decorators import login_required
 from io import BytesIO
+
+from tensorflow.python.eager.context import context
 from .models import Xray
 from .ml.predict import inception_resnt_predict_CXR_and_heatmap , prediction, diseasePredict
-
 
 def main(request):
 
@@ -13,22 +15,25 @@ def main(request):
 # # 이미지 요청 
 def test(request):
     if request.method == 'POST':
-        xray = Xray.objects.create(
-            photo = request.FILES['image'],
-        )
+        if request.user.is_authenticated:
+            xray = Xray.objects.create(
+                photo = request.FILES['image'],
+                user = request.user
+            )
+        else:
+            xray = Xray.objects.create(
+                photo = request.FILES['image'],
+            )
         return redirect('result')
 
     else:
         return render(request, 'test.html')
-    return render(request, 'test.html')
 
 
-# # 이미지 확인
+# 이미지 확인
 def result(request):
     xray = Xray.objects.last()
-    
-    # predict1 = prediction(xray.photo.path)
-    # predict3 = diseasePredict(xray.photo.path)
+
     if not xray.prediction:
         predict, heatmap, plot = inception_resnt_predict_CXR_and_heatmap(xray.photo.path)
         heatmap_io = BytesIO()
@@ -50,8 +55,6 @@ def result(request):
         'heatmap' : xray.heatmap,
         'plot' : xray.plot,
         'created_at': xray.created_at,
-        # 작성 날짜
-        # 유저
     }
 
     return render(request, 'result.html', context)
@@ -59,3 +62,28 @@ def result(request):
 def model(request):
 
     return render(request, 'model.html')
+
+@login_required
+def history(request, user_id):
+    if request.user.id != user_id:
+        return redirect('main')
+    else:
+        xrays = Xray.objects.filter(user_id=user_id)
+        context = {
+            'user': request.user,
+            'xrays': xrays
+        }
+
+    return render(request, 'history.html', context)
+
+@login_required
+def detail(request, user_id, id):
+    if request.user.id != user_id:
+        return redirect('main')
+    else:
+        xray = Xray.objects.get(id=id)
+        context = {
+            'xray': xray
+        }
+
+    return render(request, 'detail.html', context)
