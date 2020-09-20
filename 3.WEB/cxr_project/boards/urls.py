2372f94d02395eb92f996aa69c14219a85e617b1
superjuyeon <smilekjy1001@naver.com>
2020-09-20 13:27:33 +0900

    modified

--- 3.WEB/cxr_project/boards/urls.py
+++ 3.WEB/cxr_project/boards/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('test/', views.test, name='test'),
     path('result/', views.result, name='result'),
+    path('model/', views.model, name='model'),
 ]
