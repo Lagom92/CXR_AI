828e5890eea886b1513fa4eaca21cc9254ac0c16
Lagom92 <jyyt0147@gmail.com>
2020-09-21 09:59:19 +0900

    Create function about screening

--- 3.WEB/cxr_project/screening/views.py
+++ 3.WEB/cxr_project/screening/views.py
@@ -1,32 +1,47 @@
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Center
+from .data_to_db.csv_to_db import get_screening
 
 def list(request):
     centers = Center.objects.all()
 
-    q = request.GET.get('q', '')
-    if q: 
-        centers = centers.filter(address__icontains=q)
+    if len(centers):
+        q = request.GET.get('q', '')
+        if q: 
+            centers = centers.filter(address__icontains=q)
 
-    # Pagination
-    if len(centers) <= 60:
-        context = {
-            'screenings': centers
-        }
+        # Pagination
+        if len(centers) <= 60:
+            context = {
+                'screenings': centers
+            }
+        else:
+            paginator = Paginator(centers, 60) 
+            page = request.GET.get('page')
+            try:
+                screenings = paginator.page(page)
+            except PageNotAnInteger:
+                screenings = paginator.page(1)
+            except EmptyPage:
+                screenings = paginator.page(paginator.num_pages)
+            
+            context = {
+                'state': False,
+                'screenings': screenings
+            }
+
+        return render(request, 'list.html', context)
     else:
-        paginator = Paginator(centers, 60) 
-        page = request.GET.get('page')
         try:
-            screenings = paginator.page(page)
-        except PageNotAnInteger:
-            screenings = paginator.page(1)
-        except EmptyPage:
-            screenings = paginator.page(paginator.num_pages)
-        
-        context = {
-            'screenings': screenings
-        }
+            get_screening()
+            
+            return redirect('list')
+        except:
 
-    return render(request, 'list.html', context)
+            context = {
+                'state': True,
+            }
+
+            return render(request, 'list.html', context)
